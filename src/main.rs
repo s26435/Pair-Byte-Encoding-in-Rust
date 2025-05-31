92d8598c45d5e3c6b8aea4404bdf2e7ceb944472
s26435 <s26435@pjwstk.edu.pl>
2025-05-31 11:23:26 +0200

    feat: multifraeding

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::{collections::{HashMap, HashSet}, fs::File};
 use std::io::{self, BufReader, BufRead, Write};
 use indicatif::ProgressBar;
 use serde::{Serialize, Deserialize};
-
+use rayon::prelude::*;
+use std::time::Instant;
 
 #[derive(Serialize, Deserialize)]
 struct Tokenizer {
@@ -23,6 +24,7 @@ impl Tokenizer {
         assert!(!self.training_data.is_empty(), "you can't train on empty data set!");
         let pb = ProgressBar::new((dict_size-self.dict.len()) as u64);
         while self.dict.len() < dict_size {
+            let start = Instant::now();
             let mut new_token_dict: HashMap<(String, String), i32> = HashMap::new();
 
             for i in 0..self.training_data.len().saturating_sub(1) {
@@ -38,22 +40,35 @@ impl Tokenizer {
                 None => break,
             };
 
-            let merged = format!("{}{}", best_token.0, best_token.1);
+            let merged = format!("{}{}", best_token.0, best_token.1).to_owned();
             self.dict.push(merged.clone());
 
-            let mut i = 0;
-            while i < self.training_data.len().saturating_sub(1) {
-                let current = (
-                    self.training_data[i].clone(),
-                    self.training_data[i + 1].clone(),
-                );
-                if current == best_token {
-                    self.training_data[i] = merged.clone();
-                    self.training_data.remove(i + 1);
-                } else {
-                    i += 1;
+            let new_vector = self.training_data.par_chunks(50_000).map(|chunk|{
+                let mut out = Vec::with_capacity(chunk.len());
+                let mut i = 0;
+                while i < chunk.len(){
+                    if i+1 < chunk.len() && &chunk[i] == &best_token.0 && &chunk[i+1] == &best_token.1{
+                        out.push(merged.clone());
+                        i+=2;
+                    }else{
+                        out.push(chunk[i].clone());
+                        i+=1;
+                    }
                 }
-            }
+                return out;
+            }).reduce(||Vec::new(),|mut left, mut right|{
+                if let (Some(l), Some(r)) = (left.last(), right.last()){
+                    if l == &best_token.0 && r == &best_token.1{
+                        left.pop();
+                        right.remove(0);
+                        left.push(merged.clone())
+                    }
+                }
+                left.extend(right);
+                left
+            },);
+            self.training_data = new_vector;
+            let duration = start.elapsed();
             pb.inc(1);
         }
     }
@@ -96,15 +111,15 @@ impl Tokenizer {
     }
 
     fn save_to_file(&self, path: &str) {
-        let json = serde_json::to_string_pretty(&self).expect("Błąd serializacji");
-        let mut file = File::create(path).expect("Błąd tworzenia pliku");
-        file.write_all(json.as_bytes()).expect("Błąd zapisu");
+        let json = serde_json::to_string_pretty(&self).expect("error during serialization");
+        let mut file = File::create(path).expect("error during creating the file");
+        file.write_all(json.as_bytes()).expect("write error");
     }
 
     fn load_from_file(path: &str) -> Self {
-        let file = File::open(path).expect("Błąd otwierania pliku");
+        let file = File::open(path).expect("error during opening file");
         let reader = BufReader::new(file);
-        serde_json::from_reader(reader).expect("Błąd deserializacji")
+        serde_json::from_reader(reader).expect("deserialization error")
     }
 }
 
@@ -128,7 +143,7 @@ fn get_seqs_from_fasta(path: &str, limit: usize) -> Vec<String> {
     return fasta;
 }
 
-fn main() {
+fn train_new_and_predict(text: &String){
     let starting_dict: Vec<String> = vec![
         "A", "R", "N", "D", "C", "E", "Q", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V",
     ]
@@ -137,18 +152,26 @@ fn main() {
     .collect();
 
     println!("Wczytywanie danych treningowych...");
-    let train_data = get_seqs_from_fasta("seq.csv", 5000);
+    let train_data = get_seqs_from_fasta("seq.csv", 50000);
     println!("Długość danych treningowych: {:?}", train_data.len());
     println!("Rozpoczynanie trenowania...");
 
     let mut tokenizer = Tokenizer::new(train_data, starting_dict);
     tokenizer.train(10000);
-
-    println!("Słownik: {:?}", tokenizer.dict);
+    let x = tokenizer.tokenize(text.clone());
+    println!("Tokeny: {:?}", x);
+    println!("Skuteczność: {:?}", (x.len() as f64)/(text.clone().chars().as_str().len() as f64));
     tokenizer.save_to_file("tokenizer.json");
+}
+
+fn load_and_predict(text: &String){
     let tokenizer: Tokenizer = Tokenizer::load_from_file("tokenizer.json");
     let text: String = "CIRACKPDLSAETPMFPGNGDEQPLTENPRKYVM".to_string();
     let x = tokenizer.tokenize(text.clone());
     println!("Tokeny: {:?}", x);
-    println!("Skuteczność: {:?}", (x.len() as f64)/(text.clone().chars().as_str().len() as f64))
+    println!("Skuteczność: {:?}", (x.len() as f64)/(text.clone().chars().as_str().len() as f64));
+}
+
+fn main() {
+    train_new_and_predict(&"AAAAAAAAAAAA".to_string());
 }
